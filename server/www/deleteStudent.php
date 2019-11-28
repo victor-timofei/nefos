e2d0cedf6226dc565bc1ca22cfd7b374707e788c
sorin94 <Victor@vtimothy.com>
2019-11-28 23:13:12 +0200

    Fixed editStudent.php and finished deleteStudent.php
    Signed-off-by: sorin94 <Victor@vtimothy.com>

--- server/www/deleteStudent.php
+++ server/www/deleteStudent.php
@@ -1,4 +1,14 @@
 <?php
+session_start();
+if (! isset( $_SESSION['user_id'] ) ){
+    header("Location: http://127.0.0.1:8889");
+}
+else {
+//    readfile('static/deleteStudent.html');
+}
+?>
+
+<?php
 
 $db_host = 'db';
 $db_user = 'user';
@@ -8,8 +18,12 @@ $db_name = 'db_test';
 if (isset($_POST['id'])){
     $id = $_POST['id'];
     $con = new mysqli($db_host, $db_user, $db_pass, $db_name);
-    $con->query('DELETE FROM Students WHERE ID=' . $id);
-    echo ('Deleted');
+    $res = $con->query('DELETE FROM Students WHERE ID=' . $id);
+    if($res){
+        $toast = '<div class="alert alert-success">Deleted!</div>';
+    }else{
+        $toast = '<div class="alert alert-danger">Delete failed!</div>';
+    }
 }
 
 ?>
@@ -17,13 +31,38 @@ if (isset($_POST['id'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
     <meta charset="UTF-8">
-    <title>Edit Student</title>
+    <title>Delete Student</title>
 </head>
 <body>
-<form action="" method="post">
-    <input type="text" name="id" placeholder="Enter ID" required>
-    <input type="submit" value="Submit">
-</form>
+<div class="container-fluid">
+    <div class="jumbotron">
+        <div class="page-header">
+            <h2>Delete Student</h2>
+        </div>
+        <br>
+        <form action="" method="post">
+            <div class="row">
+                <div class="form-group col-sm-2">
+                    <label for="id">ID</label>
+                    <input class="form-control" id="id" type="text" name="id" placeholder="Enter ID" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-sm-3">
+                    <input class="btn btn-primary form-control" type="submit" value="Submit">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+<?php echo $toast; ?>
 </body>
 </html>
