c99a2b5afee7641bb11d35e52ee7164e3526f45e
sorin94 <Victor@vtimothy.com>
2019-11-28 22:57:06 +0200

    Completed editStudent.php
    Signed-off-by: sorin94 <Victor@vtimothy.com>

--- server/www/deleteStudent.php
+++ server/www/deleteStudent.php
@@ -1,1 +1,29 @@
 <?php
+
+$db_host = 'db';
+$db_user = 'user';
+$db_pass = 'password';
+$db_name = 'db_test';
+
+if (isset($_POST['id'])){
+    $id = $_POST['id'];
+    $con = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    $con->query('DELETE FROM Students WHERE ID=' . $id);
+    echo ('Deleted');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Edit Student</title>
+</head>
+<body>
+<form action="" method="post">
+    <input type="text" name="id" placeholder="Enter ID" required>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
--- server/www/editStudent.php
+++ server/www/editStudent.php
@@ -1,1 +1,34 @@
 <?php
+session_start();
+if (! isset( $_SESSION['user_id'] ) ){
+    header("Location: http://127.0.0.1:8889");
+}
+else {
+    readfile('static/editStudent.html');
+
+    if(isset($_POST['id'])){
+
+        $edit = 'UPDATE Students SET NAME="' . $_POST['firstname'] . '", SURNAME="' .  $_POST['lastname'] . '", FATHERNAME="' . $_POST['fathername'] . '", GRADE=' . $_POST['grade'] . ',MOBILENUMBER="' . $_POST['mobile']  . '", BIRTHDAY="' . $_POST['birthday'] . '" WHERE ID=' . $_POST['id'];
+//    echo $edit;
+        $db_host = 'db';
+        $db_user = 'user';
+        $db_pass = 'password';
+        $db_name = 'db_test';
+//    $toast_fail = '<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;"><div class="toast" style="position: absolute; top: 0; right: 0;"><div class="toast-header"><img src="" class="rounded mr-2" alt="..."><strong class="mr-auto">Students edit</strong><small>Just now</small><button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="toast-body">Update failed!</div></div></div>';
+
+        $con = new mysqli($db_host, $db_user, $db_pass, $db_name);
+        $filter_Result = $con->query($edit);
+
+        if (!$filter_Result) {
+            $toast = '<div class="alert alert-danger">Update failed!</div>';
+        }
+        else{
+            $toast = '<div class="alert alert-success">Updated!</div>';
+        }
+
+
+    }
+
+
+}
+?>
